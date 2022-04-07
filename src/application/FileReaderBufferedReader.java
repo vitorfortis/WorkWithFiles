4c8028657b5b06fa02b82e5ff10d38e2c49d28ba
Vitor Alexandre Paula da Silva <85693835+vitorfortis@users.noreply.github.com>
2022-04-07 17:42:59 -0300

    Bloco try-with-resources

--- src/application/FileReaderBufferedReader.java
+++ src/application/FileReaderBufferedReader.java
@@ -8,13 +8,9 @@ public class FileReaderBufferedReader {
 	
 	public static void main(String[] args) {
 		
-		String path = "c:\\temp\\ina.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
+		String path = "c:\\temp\\in.txt";
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
 			
 			String line = br.readLine();
 			
@@ -26,19 +22,7 @@ public class FileReaderBufferedReader {
 		catch (IOException e) {
 			System.out.println("Error" + e.getMessage());
 		}
-		finally {
-			try {
-				if(br != null) {
-					br.close();
-				}
-				if(fr != null ) {
-					fr.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		
 	}
 
 }
